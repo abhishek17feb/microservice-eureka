45cf76687ae8a89d73e3c1862813de91577d1799
Abhishek_Roy1 <abhishek2097@gmail.com>
2021-07-07 12:50:29 +0530

    Eureka Client-1

---
+++ src/main/java/com/eureka/client/paymentservice/controller/PaymentController.java
@@ -0,0 +1,13 @@
+package com.eureka.client.paymentservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @GetMapping
+    public String pay(){return "Payment Successfull";}
+}
