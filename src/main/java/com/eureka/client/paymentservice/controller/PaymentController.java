2b33ea0da8190ae55855eb7f782a0964c6a11f73
Abhishek_Roy1 <abhishek2097@gmail.com>
2021-07-07 18:10:19 +0530

    Added profiles

--- src/main/java/com/eureka/client/paymentservice/controller/PaymentController.java
+++ src/main/java/com/eureka/client/paymentservice/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package com.eureka.client.paymentservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/payment")
 public class PaymentController {
 
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping
-    public String pay(){return "Payment Successfull";}
+    public String pay(){return "Payment Successfull ["+port+"]";}
 }
