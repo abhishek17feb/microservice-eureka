c7c48e2e7b665702dd35cef16b5361fa04c0732b
Abhishek_Roy1 <abhishek2097@gmail.com>
2021-07-07 12:58:15 +0530

    Changes in eureka server configuration

--- src/main/java/com/microservice/eureka/eureka/controller/HelloController.java
+++ src/main/java/com/microservice/eureka/eureka/controller/HelloController.java
@@ -1,9 +1,13 @@
 package com.microservice.eureka.eureka.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/sayHello")
+@RestController
+@Controller
+@RequestMapping("/sayHello")
 public class HelloController {
 
     @GetMapping
